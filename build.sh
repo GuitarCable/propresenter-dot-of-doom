3672c8aefb8ca117f257a58442cdf2ad51b37b2e
GuitarCable <75274065+GuitarCable@users.noreply.github.com>
2025-10-21 17:22:27 -0500

    build.sh update

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ mkdir -p vendor/bundle
 bundle config set path 'vendor/bundle'
 
 mkdir -p build
-zip -r build/dot-of-doom.zip run.rb src vendor Gemfile config.template.yml README.md > /dev/null
+zip -r build/dot-of-doom.zip run.rb src vendor Gemfile config.template.yml README.md build.sh install.sh> /dev/null
