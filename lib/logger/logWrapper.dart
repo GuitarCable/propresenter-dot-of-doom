463935767e69bf457eb478cbddafb7e37ef017e3
GuitarCable <75274065+GuitarCable@users.noreply.github.com>
2025-12-28 11:36:19 -0600

    Refactor logWrapper to improve logging setup

--- lib/logger/logWrapper.dart
+++ lib/logger/logWrapper.dart
@@ -7,32 +7,37 @@ class LogWrapper {
 
   LogWrapper() {
     this.fullLog = "";
-    this.logger = await getLogger(fullLog);
-  }
-  Future<void> setupLoggingPath(String filePath) async {
-    // Extract the directory path from the file path
-    String directoryPath = filePath.substring(0, filePath.lastIndexOf('/'));
-
-    // Create the directory recursively if it doesn't exist
-    Directory directory = Directory(directoryPath);
-    if (!(await directory.exists())) {
-      await directory.create(recursive: true);
-      print('Directory created: ${directory.path}');
-    } else {
-      print('Directory already exists: ${directory.path}');
-    }
-
-    // Create the file if it doesn't exist
-    File file = File(filePath);
-    if (!(await file.exists())) {
-      await file.create();
-      print('File created: ${file.path}');
-    } else {
-      print('File already exists: ${file.path}');
-    }
   }
 
-  Future<Logger> getLogger(String fullLog) async {
+  String getLogDump() {
+    return fullLog;
+  }
+}
+
+Future<void> setupLoggingPath(String filePath) async {
+  // Extract the directory path from the file path
+  String directoryPath = filePath.substring(0, filePath.lastIndexOf('/'));
+
+  // Create the directory recursively if it doesn't exist
+  Directory directory = Directory(directoryPath);
+  if (!(await directory.exists())) {
+    await directory.create(recursive: true);
+    print('Directory created: ${directory.path}');
+  } else {
+    print('Directory already exists: ${directory.path}');
+  }
+
+  // Create the file if it doesn't exist
+  File file = File(filePath);
+  if (!(await file.exists())) {
+    await file.create();
+    print('File created: ${file.path}');
+  } else {
+    print('File already exists: ${file.path}');
+  }
+}
+
+Future<Logger> getLogger(String fullLog) async {
     Logger.root.level = Level.ALL;
     String loggingFilePath = './logs/dot_of_doom.txt';
     await setupLoggingPath(loggingFilePath);
@@ -48,8 +53,3 @@ class LogWrapper {
 
     return logger;
   }
-
-  String getLogDump() {
-    return fullLog;
-  }
-}
